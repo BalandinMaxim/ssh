7b25e9a68bf9b72c0c9d888d45105c38e777bd31
BalandinMaxim <112868629+BalandinMaxim@users.noreply.github.com>
2023-05-14 13:23:28 +0300

    Update log.php

--- WEB5/log.php
+++ WEB5/log.php
@@ -1,76 +1,96 @@
 <?php
+
+/**
+ * Файл log.php для не авторизованного пользователя выводит форму логина.
+ * При отправке формы проверяет логин/пароль и создает сессию,
+ * записывает в нее логин и id пользователя.
+ * После авторизации пользователь перенаправляется на главную страницу
+ * для изменения ранее введенных данных.
+ **/
+
+// Отправляем браузеру правильную кодировку,
+// файл log.php должен быть в кодировке UTF-8 без BOM.
 header('Content-Type: text/html; charset=UTF-8');
+
+// Начинаем сессию.
 session_start();
-if(isset($_COOKIE["init"])){
-  setcookie("init", "", 10000);
-  $_SESSION['login']="";
-}
-if(!empty($_SESSION['login'])){
-  header('Location: index.php'); 
-  exit();
-}
+
+// В суперглобальном массиве $_SERVER PHP сохраняет некторые заголовки запроса HTTP
+// и другие сведения о клиненте и сервере, например метод текущего запроса $_SERVER['REQUEST_METHOD'].
 if ($_SERVER['REQUEST_METHOD'] == 'GET') {
+  if (!empty($_SESSION['login'])) {
+  header('Location: index.php');
+  }else{
 ?>
-<!DOCTYPE html>
-<html lang="ru">
-  <head>
-    <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link rel="stylesheet" href="log_style.css" />
-    <title>Задание 5</title>
-  </head>
-  <body>
-    <form method="POST" action="log.php">
-    <div class="container">
-      <div style="margin-bottom: 20px; color:white; <?php
-      if(!isset($_COOKIE["info"])){
-        echo "display: none;";
-      }
-      ?>">Ввелен не верный логин или пароль</div>
-    <div class="inputs">
-        <label style="margin-left:15px; font-size: 20px; ">Логин</label>
-        <input type="text" name="login" placeholder="Введите логин" value="<?php
-        if(isset($_COOKIE["login"])){
-          echo $_COOKIE["login"];
-        }
-        ?>" required/>
-        <label style="margin-left:15px; font-size: 20px; ">Пароль</label>
-        <input type="password" name="password" placeholder="Введите пароль" value="<?php
-        if(isset($_COOKIE["password"])){
-          echo $_COOKIE["password"];
-        }
-        ?>" required/>
-        <button type="submit">Вход</button>
-    </div>
-    <a href="index.php">Вход без авторизации</a>
-    </div>
-  </form>
-  </body>
-</html>
-
-<?php
-}
-else {
-  $user = 'u52884'; $pass = '6854641';
-  $con = new PDO('mysql:host=localhost;dbname=u52879', $user, $pass, [PDO::ATTR_PERSISTENT => true]);
-  $login=$_POST["login"];
-  $password=$_POST["password"];
-  $password_time=md5($password);
-  $sth = $con->prepare("SELECT id, login, password, id_f FROM USER where login=:login and password=:password");
-  $sth->execute(['login'=>"$login", 'password'=>"$password_time"]);
-  $result = $sth->fetchAll();
-  setcookie("login", $login);
-  setcookie("password", $password);
-  setcookie("info", "error");
-
-  if(!empty($result)){
-    $_SESSION['login'] = $login;
-    $_SESSION['id'] = $result[0]['id'];
-    $_SESSION['id_f']=$result[0]['id_f'];
-    $_SESSION['password']=$password;
-    setcookie("info", "", 10000);
+<style>
+  body{
+     background-image: url("2.jpg");
+     background-size: no-repeat;
+     display: block;
+     justify-content:center;
   }
-  header('Location: login.php');
-  exit();
+	
+  .button {
+     text-align: center;
+  }
+	
+  .log-in{
+    font-family: "Montserrat", sans-serif;
+    max-width: 960px;
+    margin: 20% auto;
+    padding: 30px 40px 5px 40px;
+    width: 250px;
+    background-color: #95bade;
+    border: 2px solid #26527C;
+  }
+</style>
+<div class="log-in">
+<form action="log.php" method="post">
+  <input name="login" /> Логин<br><br>
+  <input name="password" type="password"/> Пароль<br>
+  <p class = "button">
+     <input type="submit" value="Войти" />
+  </p>
+</form>
+</div>
+<?php
+  }
 }
-?>
+// Иначе, если запрос был методом POST, т.е. нужно сделать авторизацию с записью логина в сессию.
+else {
+  $login=$_POST['login'];
+  $pswrd=$_POST['password'];
+  $uid=0;
+  $error=TRUE;
+  $user = 'u52884';
+  $pass = '6854641';
+  $db1 = new PDO('mysql:host=localhost;dbname=u52884', $user, $pass, array(PDO::ATTR_PERSISTENT => true));
+  if(!empty($login) and !empty($pswrd)){
+    try{
+      $chk=$db1->prepare("SELECT * FROM USER WHERE login=?");
+      $chk->bindParam(1,$login);
+      $chk->execute();
+      $username=$chk->fetchALL();
+	  print($username[0]['password']);
+      if(password_verify($pswrd,$username[0]['password'])){
+        $uid=$username[0]['id'];
+        $error=FALSE;
+      }
+    }
+    catch(PDOException $e){
+      print('Error : ' . $e->getMessage());
+      exit();
+    }
+  }
+  if($error==TRUE){
+    print('Неправильные логин или пароль? <br> Создайте нового <a href="index.php">пользователя</a> или <a href="log.php">попробовать войти снова</a> ');
+    session_destroy();
+    exit();
+  }
+  // Если все ок, то авторизуем пользователя.
+  $_SESSION['login'] = $login;
+  // Записываем ID пользователя.
+  $_SESSION['uid'] = $uid;
+  // Делаем перенаправление.
+  header('Location: index.php');
+}
