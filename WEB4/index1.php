09a4da37baa1de65c4d59a6059f7c70c85023076
BalandinMaxim <112868629+BalandinMaxim@users.noreply.github.com>
2023-05-20 14:20:33 +0300

    Update index1.php

--- WEB4/index1.php
+++ WEB4/index1.php
@@ -1,7 +1,207 @@
 <?php
 header('Content-Type: text/html; charset=UTF-8');
 function namet($data){
-  $pattern = '/^[а-яёЁА-Я]+$/u';
+  $pattern = '/<?php
+// Отправляем браузеру правильную кодировку,
+// файл index.php должен быть в кодировке UTF-8 без BOM.
+header('Content-Type: text/html; charset=UTF-8');
+
+// В суперглобальном массиве $_SERVER PHP сохраняет некторые заголовки запроса HTTP
+// и другие сведения о клиненте и сервере, например метод текущего запроса $_SERVER['REQUEST_METHOD'].
+if ($_SERVER['REQUEST_METHOD'] == 'GET') {
+         $messages = array();
+
+
+  // В суперглобальном массиве $_GET PHP хранит все параметры, переданные в текущем запросе через URL.
+   if (!empty($_COOKIE['save'])) {
+    // Удаляем куку, указывая время устаревания в прошлом.
+    setcookie('save', '', 100000);
+    // Если есть параметр save, то выводим сообщение пользователю.
+    $messages[] = 'Спасибо, результаты сохранены.';
+  }
+  
+  $errors = array();
+  $errors['fio'] = !empty($_COOKIE['fio_error']);
+  $errors['email'] = !empty($_COOKIE['email_error']);
+  $errors['date'] = !empty($_COOKIE['date_error']);
+  $errors['sex'] = !empty($_COOKIE['sex_error']);
+  $errors['limbs'] = !empty($_COOKIE['limbs_error']);
+  $errors['abilities'] = !empty($_COOKIE['abilities_error']);
+  $errors['bio'] = !empty($_COOKIE['bio_error']);
+  $errors['checkbox'] = !empty($_COOKIE['checkbox_error']);
+  
+  if ($errors['fio']) {
+    // Удаляем куку, указывая время устаревания в прошлом.
+    setcookie('fio_error', '', 100000);
+    // Выводим сообщение.
+    $messages[] = '<div class="error">Заполните имя. <br> Используйте символы: А-Я, а-я, A-Z,a-z</div>';
+  }
+  if ($errors['email']) {
+    setcookie('email_error', '', 100000);
+    $messages[] = '<div class="error">Заполните email.</div>';
+  }
+  if ($errors['date']) {
+    setcookie('date_error', '', 100000);
+    $messages[] = '<div class="error">Заполните дату рождения. <br> Должен содержать @</div>';
+  }
+  if ($errors['sex']) {
+    setcookie('sex_error', '', 100000);
+    $messages[] = '<div class="error">Укажите пол. <br> день/месяц/год </div>';
+  }
+  if ($errors['limbs']) {
+    setcookie('limbs_error', '', 100000);
+    $messages[] = '<div class="error">УУкажите количество конечностей.</div>';
+  }
+  if ($errors['abilities']) {
+    setcookie('abilities_error', '', 100000);
+    $messages[] = '<div class="error">Укажите сверхспособность.</div>';
+  }
+  if ($errors['bio']) {
+    setcookie('bio_error', '', 100000);
+    $messages[] = '<div class="error">Заполните биографию. <br> Используйте символы: А-Я, а-я, A-Z,a-z</div>';
+  }
+  if ($errors['checkbox']) {
+    setcookie('checkbox_error', '', 100000);
+    $messages[] = '<div class="error">Примите условия соглашения.</div>';
+  }
+  
+  $values = array();
+  $values['fio'] = empty($_COOKIE['fio_value']) ? '' : $_COOKIE['fio_value'];
+  $values['email'] = empty($_COOKIE['email_value']) ? '' : $_COOKIE['email_value'];
+  $values['date'] = empty($_COOKIE['date_value']) ? '' : $_COOKIE['date_value'];
+  $values['sex'] = empty($_COOKIE['sex_value']) ? '' : $_COOKIE['sex_value'];
+  $values['limbs'] = empty($_COOKIE['limbs_value']) ? '' : $_COOKIE['limbs_value'];
+  $values['abilities'] = empty($_COOKIE['abilities_value']) ? '' : $_COOKIE['abilities_value'];
+  $values['bio'] = empty($_COOKIE['bio_value']) ? '' : $_COOKIE['bio_value'];
+  $values['checkbox'] = empty($_COOKIE['checkbox_value']) ? '' : $_COOKIE['checkbox_value'];
+  
+  // Включаем содержимое файла form.php.
+  include('form.php');
+  
+}
+// Иначе, если запрос был методом POST, т.е. нужно проверить данные и сохранить их в XML-файл.
+else{
+// Проверяем ошибки.
+$errors = FALSE;
+if (empty($_POST['fio'])) {
+    // Выдаем куку на день с флажком об ошибке в поле fio.
+    setcookie('fio_error', '1', time() + 24 * 60 * 60);
+    $errors = TRUE;
+  }
+  else {
+    // Сохраняем ранее введенное в форму значение на год.
+    setcookie('fio_value', $_POST['fio'], time() + 30 * 24 * 60 * 60 * 12);
+  }
+
+  if (empty($_POST['email'])) {
+    setcookie('email_error', '1', time() + 24 * 60 * 60);
+    $errors = TRUE;
+  }
+  else {
+    setcookie('email_value', $_POST['email'], time() + 30 * 24 * 60 * 60 * 12);
+  }
+
+  if (empty($_POST['date'])) {
+    setcookie('date_error', '1', time() + 24 * 60 * 60);
+    $errors = TRUE;
+  }
+  else {
+    setcookie('date_value', $_POST['date'], time() + 30 * 24 * 60 * 60 * 12);
+  }
+
+  if (empty($_POST['sex'])) {
+    setcookie('sex_error', '1', time() + 24 * 60 * 60);
+    $errors = TRUE;
+  }
+  else {
+    setcookie('sex_value', $_POST['sex'], time() + 30 * 24 * 60 * 60 * 12);
+  }
+
+  if (empty($_POST['limbs'])) {
+    setcookie('limbs_error', '1', time() + 24 * 60 * 60);
+    $errors = TRUE;
+  }
+  else {
+    setcookie('limbs_value', $_POST['limbs'], time() + 30 * 24 * 60 * 60 * 12);
+  }
+
+  if (empty($_POST['abilities'])) {
+    setcookie('abilities_error', '1', time() + 24 * 60 * 60);
+    $errors = TRUE;
+  }
+  else {
+    setcookie('abilities_value', $_POST['abilities'], time() + 30 * 24 * 60 * 60 * 12);
+  }
+
+  if (empty($_POST['bio'])) {
+    setcookie('bio_error', '1', time() + 24 * 60 * 60);
+    $errors = TRUE;
+  }
+  else {
+    setcookie('bio_value', $_POST['bio'], time() + 30 * 24 * 60 * 60 * 12);
+  }
+
+  if (empty($_POST['checkbox'])) {
+    setcookie('checkbox_error', '1', time() + 24 * 60 * 60);
+    $errors = TRUE;
+  }
+  else {
+    setcookie('checkbox_value', $_POST['checkbox'], time() + 30 * 24 * 60 * 60 * 12);
+  }
+
+
+if ($errors) {
+  // При наличии ошибок завершаем работу скрипта.
+  header('Location: index1.php');
+  exit();
+}
+  else {
+    // Удаляем Cookies с признаками ошибок.
+    setcookie('fio_error', '', 100000);
+    setcookie('email_error', '', 100000);
+    setcookie('date_error', '', 100000);
+    setcookie('sex_error', '', 100000);
+    setcookie('limbs_error', '', 100000);
+    setcookie('abilities_error', '', 100000);
+    setcookie('bio_error', '', 100000);
+    setcookie('checkbox_error', '', 100000);
+  }
+
+
+// Сохранение в базу данных.
+
+$user = 'u52884';
+$pass = '6854641';
+$db = new PDO('mysql:host=localhost;dbname=u52884', $user, $pass,
+[PDO::ATTR_PERSISTENT => true, PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]); // Заменить test на имя БД, совпадает с логином uXXXXX
+
+// Подготовленный запрос. Не именованные метки.
+try {
+
+  $stmt = $db->prepare("INSERT INTO FORMS_CHAR (name,email,year,gender,limbs,biography) VALUES 
+  (?,?,?,?,?,?,?)");
+  $stmt -> execute([$_POST['fio'], $_POST['email'], $_POST['date'], $_POST['sex'], $_POST['limbs'], $_POST['bio']]);
+  $id = $db->lastInsertId();
+  $stmt = $db->prepare("INSERT INTO FORM_SUPER_CHAR (id_DATA_FORM, id_DATA_superpower) VALUES (?,?)");
+    foreach ($_POST['abilities'] as $ability) {
+          $stmt->execute([$id, $ability]);
+        }
+
+
+
+ 
+}
+catch(PDOException $e){
+  print('Error : ' . $e->getMessage());
+  exit();
+}
+
+ // Сохраняем куку с признаком успешного сохранения.
+  setcookie('save', '1');
+
+  // Делаем перенаправление.
+  header('Location: index1.php');
+}^[а-яёЁА-Я]+$/u';
   if(preg_match($pattern, $data))
     return 1;
   else
